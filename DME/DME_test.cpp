84c3b18225dbd2e544aed7bc55a967f4c2bd1864
geziangbuyiding <1041143432@qq.com>
2024-01-31 23:38:31 +0800

    fixed path problem

--- DME/DME_test.cpp
+++ DME/DME_test.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     ISPD2009Parser parser;
-    CTSDB* ctsdb=new CTSDB();
+    CTSDB *ctsdb = new CTSDB();
     gArg.Init(argc, argv);
     if (argc < 2)
     {
         return 0;
     }
-    if (strcmp(argv[1] + 1, "input") == 0) // -aux, argv[1]=='-'
+    if (strcmp(argv[1] + 1, "input") == 0) // -input, argv[1]=='-'
     {
         // bookshelf
         printf("Use ISPD2009 format\n");
@@ -25,13 +25,36 @@ int main(int argc, char *argv[])
             printf("    Path = %s\n", filename.substr(0, pos + 1).c_str());
             gArg.Override("path", filename.substr(0, pos + 1));
         }
+        string benchmarkName;
+        int length = filename.length();
+        benchmarkName = filename.substr(pos + 1, length - pos);
+        gArg.Override("benchmarkName", benchmarkName);
+        cout << "    Benchmark: " << benchmarkName << endl;
+
+        string plotPath;
+
+        if (!gArg.GetString("plotPath", &plotPath))
+        {
+            plotPath = "./";
+        }
+
+        plotPath += "/" + benchmarkName + "/";
+
+        string cmd = "rm -rf " + plotPath;
+        system(cmd.c_str());
+        cmd = "mkdir -p " + plotPath;
+        system(cmd.c_str());
+
+        gArg.Override("plotPath", plotPath);
+
+        cout << "    Plot path: " << plotPath << endl;
 
         parser.ReadFile(argv[2], ctsdb);
     }
     ctsdb->showCTSdbInfo();
-    TreeTopology* topo=new TreeTopology(ctsdb);
+    TreeTopology *topo = new TreeTopology(ctsdb);
     topo->buildTopoUsingNearestNeighborGraph();
-    ZSTDMERouter* router=new ZSTDMERouter(ctsdb);
+    ZSTDMERouter *router = new ZSTDMERouter(ctsdb);
     router->setDelayModel(ELMORE_DELAY);
     router->setTopology(topo);
     router->ZSTDME();
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## ZST/DME
 ### refer to the book: Electronic Design Automation: Synthesis, Verification, and Test
 ### or to the paper by abk: Zero skew clock routing with minimum wirelength 
-### todo: snaking in top down!!!!
+### todo: snaking in top down!!!! and better plotting
